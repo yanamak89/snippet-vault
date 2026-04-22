21fee7358686fac98d11d4297a4252d28a954134
Yana Makogon <yana.mak89@gmail.com>
2026-04-22 18:52:34 +0200

    fix: allow cors for deployed frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   });
 
   app.useGlobalPipes(
